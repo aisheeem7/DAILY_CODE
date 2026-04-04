62814b56cd4e31669ce97ba1fab1a726da7f1c0c
Aishee Mukherjee <aisheeur7@gmail.com>
2026-04-04 11:30:40 +0530

    Create nonrep.java

---
+++ strings/nonrep.java
@@ -0,0 +1,14 @@
+class nonrep {
+    static char firstNonRepeat(String s) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+
+        for (char c : s.toCharArray())
+            if (freq[c - 'a'] == 1)
+                return c;
+
+        return '#';
+    }
+}
